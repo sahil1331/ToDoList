b401618cd335a047d32dc102a4d529e100602d90
Sahil <sahilisin11@gmail.com>
2022-08-01 01:32:53 +0530

    added todo delete feature

--- assets/app.js
+++ assets/app.js
@@ -34,3 +34,10 @@ addForm.addEventListener('submit',(e)=>{
  
 })
 
+//delete todos
+
+list.addEventListener('click',(e)=>{
+if(e.target.classList.contains('delete')){
+    e.target.parentElement.remove();
+}
+})
